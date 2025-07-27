9cf0c83299a0fa7f6f7443cafc180258b5bb98c1
Ritesh Das <riteshdas8652@gmail.com>
2025-07-27 19:33:15 +0530

    course model change

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -5,7 +5,7 @@ const courseSchema = mongoose.Schema({
     description:{type:String, required:true},
     price:{type:Number, required:true},
     category:{type:String, required: true},
-    level:{type:String, enum:['beginner','medium','advance'],required:true},
+    level:{type:String, enum:['beginner','intermediate','advance'],required:true},
     thumbnail:{type:String, default:''},
     instructor:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
     lessons:[{type:mongoose.Schema.Types.ObjectId, ref:'Lesson'}],
