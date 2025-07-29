d7cbdb7354548e7510a16ba852fb77c16c8744b2
Ritesh Das <riteshdas8652@gmail.com>
2025-07-29 19:23:21 +0530

    image

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const {createCourse, getAllCourse, getInstructorCourse, updateCourse, deleteCourse} = require('../controllers/course.controller');
 const isAuthenticated = require('../middleware/authenticated');
-const checkRole = require('../middleware/checkRole');
+const singleUpload = require('../middleware/multer');
 
-router.post('/createcourse',isAuthenticated,checkRole(['instructor']), createCourse);
-router.get('/get',isAuthenticated,checkRole(['student']),getAllCourse);
-router.get('/get/instructorcourse/:id',isAuthenticated,checkRole(['instructor']),getInstructorCourse);
-router.put('/update/:id',isAuthenticated,checkRole(['instructor']),updateCourse);
-router.delete('/delete/:id',isAuthenticated,checkRole(['instructor']),deleteCourse);
+router.post('/createcourse',isAuthenticated, singleUpload,createCourse);
+router.get('/get',isAuthenticated,getAllCourse);
+router.get('/get/instructorcourse/:id',isAuthenticated,getInstructorCourse);
+router.put('/update/:id',isAuthenticated,updateCourse);
+router.delete('/delete/:id',isAuthenticated,deleteCourse);
 
 module.exports = router;
