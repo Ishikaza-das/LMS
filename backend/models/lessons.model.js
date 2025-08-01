e4263dccf096338e58bb365be86b083badb8cead
Ritesh Das <riteshdas8652@gmail.com>
2025-08-01 10:28:29 +0530

    lesson status

--- backend/models/lessons.model.js
+++ backend/models/lessons.model.js
@@ -4,6 +4,7 @@ const lessonSchema = mongoose.Schema({
     courseId:{type:mongoose.Schema.Types.ObjectId, ref:'Course', required:true},
     title:{type:String, required:true},
     content:{type:String},
+    status:{type:String, enum:["public","private"],require:true, default:"private"},
     videoUrl:{type:String, required:true},
     order:{type:Number},
     quiz:{type:mongoose.Schema.Types.ObjectId, ref:'Quiz',default:null}
