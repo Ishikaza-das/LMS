711aeb2c4ce8d35831361b0da9e5ed0e67227b3a
Ritesh Das <riteshdas8652@gmail.com>
2025-09-10 19:46:47 +0530

    Student UI

--- frontend/src/components/pages/components/LessonBox.jsx
+++ frontend/src/components/pages/components/LessonBox.jsx
@@ -1,25 +1,31 @@
 import { PlayCircle } from "lucide-react";
 import React, { useState } from "react";
 
-const LessonBox = ({course}) => {
+const LessonBox = ({ course }) => {
   const [currentVideo, setCurrentVideo] = useState(null);
   return (
     <div className="flex flex-col xl:flex-row gap-10 pt-4">
       <div className="w-full xl:w-1/2 pt-4">
-        <video className="w-full md:h-[300px] lg:h-[400px] bg-amber-400">
-        </video>
+        {currentVideo ?
+          (<video className="w-full md:h-[300px] lg:h-[400px]" controls src={currentVideo}/>):(
+            <div className="w-full md:h-[300px] lg:h-[400px] bg-gray-800 flex items-center justify-center">
+              <p className="">No Video Selcted</p>
+            </div>
+          )}
       </div>
       <div className="w-full xl:w-1/2 space-y-8 pt-4">
-      {
-        course?.lessons?.map((lecture,index) => (
-          <div key={index} className="bg-gray-800 p-4 rounded-md">
+        {course?.lessons?.map((lecture, index) => (
+          <div
+            key={index}
+            className="bg-gray-800 p-4 rounded-md"
+            onClick={() => setCurrentVideo(lecture?.videoUrl)}
+          >
             <div className="flex gap-4 cursor-pointer">
-              <PlayCircle/>
+              <PlayCircle />
               <h1>{lecture?.title}</h1>
             </div>
           </div>
-        ))
-      } 
+        ))}
       </div>
     </div>
   );
--- frontend/src/components/pages/student/Profile.jsx
+++ frontend/src/components/pages/student/Profile.jsx
@@ -1,38 +1,37 @@
 import Navbar from "@/components/shared/Navbar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Mail, Pen } from "lucide-react";
+import { Pen } from "lucide-react";
 import React from "react";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const { user } = useSelector(store => store.auth);
+  const { user } = useSelector((store) => store.auth);
+
   return (
-    <div>
+    <div className="min-h-screen bg-background">
       <Navbar />
-      <div className="max-w-7xl mx-auto my-4 p-2 bg-white border border-gray-300 rounded-2xl">
-        <div className=" flex justify-between">
-          <div className="flex items-center gap-10">
-            <Avatar className="h-30 w-30">
-              <AvatarImage
-                className="w-full object-cover"
-                src={user?.profilephoto || "https://github.com/shadcn.png"}
-              />
-            </Avatar>
-            <div>
-              <h1 className="font-medium text-xl">{user?.fullname}</h1>
+      <div className="max-w-7xl mx-auto my-6 px-4">
+        <div className="p-6 bg-accent-foreground rounded-2xl shadow-lg">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-6">
+              <Avatar className="h-20 w-20 xl:h-28 xl:w-28 shadow-md">
+                <AvatarImage
+                  className="w-full h-full object-cover rounded-full"
+                  src={user?.profilephoto || "https://github.com/shadcn.png"}
+                />
+              </Avatar>
+              <div>
+                <h1 className="font-bold text-2xl text-white">{user?.fullname}</h1>
+                <p className="text-gray-300">{user?.email}</p>
+              </div>
             </div>
-          </div>
-          <Button className="text-right border border-blue-600" variant="outline"><Pen/></Button>
-        </div>
-        <div className="my-6 px-4">
-          <div className="flex items-center gap-4">
-            <Mail/>
-            <p>{user?.email}</p>
+            <Button className="bg-blue-600 hover:bg-blue-700 rounded-xl shadow-md">
+              <Pen />
+            </Button>
           </div>
         </div>
       </div>
-      {/* Purchase */}
     </div>
   );
 };
